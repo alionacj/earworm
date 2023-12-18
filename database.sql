94664cc0a7332213f361baa0f4a7403ec019f5dd
Leo Johnson <leochinonejohnson@gmail.com>
2023-12-18 13:51:02 -0600

    saved establishing db queries

--- database.sql
+++ database.sql
@@ -1,10 +1,31 @@
+DROP TABLE IF EXISTS "users", "session", "session_settings", "session_intervals";
 
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
-CREATE TABLE "user" (
+CREATE TABLE "users" (
     "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "username" VARCHAR(200),
+    "password" TEXT,
+    "created_at" TIMESTAMPTZ
+);
+
+CREATE TABLE "session" (
+    "id" SERIAL PRIMARY KEY,
+    "session_number" INTEGER,
+    "session_date" TIMESTAMPTZ,
+    "comments" TEXT,
+    "user_id" INT
+    	REFERENCES "users"
+);
+
+CREATE TABLE "session_settings" (
+	"id" SERIAL PRIMARY KEY,
+	"playback_type" VARCHAR(100),
+	"sound_type" VARCHAR (200),
+	"session_id" INT REFERENCES "session"
+);
+
+CREATE TABLE "session_intervals" (
+	"id" SERIAL PRIMARY KEY,
+	"interval_type" VARCHAR(50),
+	"is_correct" BOOLEAN,
+	"session_id" INT REFERENCES "session"
 );
