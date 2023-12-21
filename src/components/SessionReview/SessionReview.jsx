0cdb5e85f6a46f21274f28732c3ba2072ab32893
Leo Johnson <leochinonejohnson@gmail.com>
2023-12-21 17:06:34 -0600

    created session review skeleton

---
+++ src/components/SessionReview/SessionReview.jsx
@@ -0,0 +1,23 @@
+
+
+function SessionReview() {
+
+    return (
+        <>
+            <h3>REVIEW</h3>
+            <div>
+                <p>SESSION X COMPLETED</p>
+                <p>AVERAGE CORRECT: X%</p>
+                <p>HIGHEST SCORING: X X%</p>
+                <p>LOWEST SCORING: X X%</p>
+                <p>interval scores dropdown</p>
+                <button>VIEW SESSIONS</button>
+            </div>
+                <br></br><br></br>
+            <button>EXIT</button>
+            <button>RETRY</button>
+        </>
+    )
+}
+
+export default SessionReview
