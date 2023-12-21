89eb4a3a2b7426b6626c85b8e5baf5377f7b9fbf
Leo Johnson <leochinonejohnson@gmail.com>
2023-12-21 17:07:02 -0600

    added div

--- src/components/Session/Session.jsx
+++ src/components/Session/Session.jsx
@@ -8,19 +8,21 @@ function Session() {
             {/* play switches to stop while playing */}
             <button>▶️</button>
             <h3>SELECT ANSWER</h3>
-            <button>U</button>
-            <button>m2</button>
-            <button>M2</button>
-            <button>m3</button>
-            <button>M3</button>
-            <button>P4</button>
-            <button>TT</button>
-            <button>P5</button>
-            <button>m6</button>
-            <button>M6</button>
-            <button>m7</button>
-            <button>M7</button>
-            <button>8ve</button>
+            <div>
+                <button>U</button>
+                <button>m2</button>
+                <button>M2</button>
+                <button>m3</button>
+                <button>M3</button>
+                <button>P4</button>
+                <button>TT</button>
+                <button>P5</button>
+                <button>m6</button>
+                <button>M6</button>
+                <button>m7</button>
+                <button>M7</button>
+                <button>8ve</button>
+            </div>
             <button>EXIT</button>
             <button>NEXT</button>
         </>
