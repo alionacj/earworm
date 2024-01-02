a172af88d9729b5efbbf9d5769ac4bf167a41b96
Leo Johnson <leochinonejohnson@gmail.com>
2024-01-02 15:09:23 -0600

    refactored interval rendering

--- notes.md
+++ notes.md
@@ -6,8 +6,12 @@ session options
     - looks like I can do this with MUI. I didnt want to do 
       any styling but it might be necessary to install it now
 - (-) change playback to a radial
-- put whatever info into a reducer via saga & send to db
+- (X) put whatever info into a reducer via saga & send to db
     - user sends an input ->
     - saga is triggered ->
     - saga function sends a post to db ->
-    - 
+
+session
+- () trigger sounds per question...
+- () figure out random interval selection
+- () POSTs to db for each question
--- src/components/Session/Session.jsx
+++ src/components/Session/Session.jsx
@@ -7,8 +7,12 @@ function Session() {
     const history = useHistory()
     const dispatch = useDispatch()
 
+    // settings data from reducer
     const settings = useSelector(store => store.settings)
+    const intervals = settings.intervals
+    const playback = settings.playback
 
+    // gets latest settings from db
     useEffect(() => {
         dispatch({
             type: 'FETCH_SETTINGS'
@@ -16,6 +20,11 @@ function Session() {
         console.log('settings:', settings)
     }, [])
 
+    const handleAnswer = (interval) => {
+        console.log(interval)
+    }
+
+    // navigation
     const exit = () => {
         history.push('/home')
     }
@@ -29,22 +38,15 @@ function Session() {
             {/* play switches to stop while playing */}
             <button>▶️</button>
             <h3>SELECT ANSWER</h3>
-            <div>
-                <button>U</button>
-                <button>m2</button>
-                <button>M2</button>
-                <button>m3</button>
-                <button>M3</button>
-                <button>P4</button>
-                <button>TT</button>
-                <button>P5</button>
-                <button>m6</button>
-                <button>M6</button>
-                <button>m7</button>
-                <button>M7</button>
-                <button>8ve</button>
-            </div>
-                <br/>
+            {intervals &&
+            intervals.map(interval =>
+                <button
+                    key={intervals.indexOf(interval)}
+                    onClick={() => handleAnswer(interval)}
+                >
+                    {interval}
+                </button>)}
+            <br/><br/>
             <button onClick={exit}>EXIT</button>
             {/* placeholder to review - will eventually move to next question */}
             <button onClick={next}>NEXT</button>
