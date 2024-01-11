4a0f67eea30f89a907b7a3cd1de59fa328613886
Leo Johnson <leochinonejohnson@gmail.com>
2024-01-10 20:25:49 -0600

    completed sessionhistory page

--- src/components/SessionHistory/SessionHistory.jsx
+++ src/components/SessionHistory/SessionHistory.jsx
@@ -1,8 +1,26 @@
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min"
+
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+
 
 function SessionHistory() {
 
     const history = useHistory()
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch({
+            type: 'FETCH_HISTORY'
+        })
+    }, [])
+
+    const userHistory = useSelector(store => store.history)
+    console.log(userHistory)
+
 
     const back = () => {
         history.push('/review')
@@ -12,10 +30,22 @@ function SessionHistory() {
     }
 
     return (
+        userHistory[0] &&
         <>
             <h3>SESSION HISTORY</h3>
-            <div>
-                <p>sessions go here</p>
+            <div style={{maxHeight: 500, overflow: 'auto'}}>
+                {userHistory.map((session) => (
+                    <Accordion>
+                        <AccordionSummary>
+                            Session {session.session_number}
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            {session.intervals.map((int) => (
+                                <p>{int.interval}: {int.correct}/{Number(int.correct) + Number(int.incorrect)}</p>
+                            ))}
+                        </AccordionDetails>
+                    </Accordion>
+                ))}
             </div>
             <button onClick={back}>BACK</button>
             <button onClick={exit}>EXIT</button>
